501da1228ec38a0ff5075015fcac62a92dca56b6
zwxxzhl <csdhtt@foxmail.com>
2021-04-07 10:14:35 +0800

    .

--- cssysft-2146/src/main.js
+++ cssysft-2146/src/main.js
@@ -3,7 +3,7 @@ import App from '@/App.vue'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css'
-import locale from 'element-plus/lib/locale/lang/zh-cn';
+import locale from 'element-plus/lib/locale/lang/zh-cn'
 
 import router from '@/router'
 import store from '@/store'
