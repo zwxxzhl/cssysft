9f27688accfcd83bbef3b329df5f2c7d8e15391b
zwxxzhl <147225922@qq.com>
2021-05-20 23:13:25 +0800

    .

--- cssysft-2146/src/views/activiti/task_todo/list.vue
+++ cssysft-2146/src/views/activiti/task_todo/list.vue
@@ -80,9 +80,156 @@
   </div>
 </template>
 
-<script>
-import {mapGetters} from "vuex";
+<script setup>
+import BpmnJs from "@/components/BpmnJs/index.vue";
+import { defineProps, ref, reactive, onMounted } from 'vue'
 
+const listLoading = ref(true);
+const list = reactive(null);
+const total = ref(0);
+const page = ref(1);
+const limit = ref(10);
+const searchObj = reactive({});
+const multipleSelection = reactive([]);
+
+const bpmnVisible = ref(false);
+const ifBpmnAdd = ref(true);
+const bpmnData = reactive(null);
+
+//启动子流程实例
+const onStartSubInstance = (row) => {
+  activitiApi.startSubInstance({
+    key: row.processDefinitionKey,
+    procinstId: row.processInstanceId,
+    name: row.name,
+    variable: '自定义变量'
+  }).then(res => {
+    if (res.success) {
+      this.$message({
+        type: 'success',
+        message: res.message
+      })
+    }
+  })
+};
+//完成任务
+const onComplete = (row) => {
+  activitiApi.completeTask(
+    row.id
+  ).then(res => {
+    if (res.success) {
+      this.fetchData();
+      this.$message({
+        type: 'success',
+        message: res.message
+      })
+    }
+  })
+};
+//导出svg
+const onExportImg = () => {
+  this.$refs.refBpmnJs.exportImg();
+};
+//导出bpmn
+const onExportBpmn = () => {
+  this.$refs.refBpmnJs.exportBpmn();
+};
+//前进
+const onForward = () => {
+  this.$refs.refBpmnJs.forward();
+};
+//后退
+const onRetreat = () => {
+  this.$refs.refBpmnJs.retreat();
+};
+//bpmn模态框打开事件
+const onOpened = () => {
+  this.$refs.refDialogDiv.style.height = parent.innerHeight * 0.7 + "px";
+  if (this.ifBpmnAdd) {
+    this.$refs.refBpmnJs.newDiagram();
+  } else {
+    this.$refs.refBpmnJs.viewColor(this.bpmnData);
+  }
+};
+//关闭bpmn
+const onCancel = () => {
+  this.bpmnVisible = false;
+};
+//创建bpmn
+const onOpenBpmn = () => {
+  this.ifBpmnAdd = true;
+  this.bpmnVisible = true;
+};
+//部署流程
+const onDeploy = () => {
+  this.$refs.refBpmnJs.deploy();
+};
+//查看流程
+const onViewBpmn = (row) => {
+  this.ifBpmnAdd = false;
+  this.bpmnData = row;
+  this.bpmnVisible = true;
+};
+const onDeleteBpmn = (row) => {
+  this.$confirm("此操作将删除流程, 是否继续?", "提示", {
+    confirmButtonText: "确定",
+    cancelButtonText: "取消",
+    type: "warning",
+  })
+    .then(() => {
+      // promise
+      // 点击确定，远程调用ajax
+      return userApi.removeById(id);
+    })
+    .then((response) => {
+      this.fetchData(this.page);
+      if (response.success) {
+        this.$message({
+          type: "success",
+          message: "删除成功!",
+        });
+      }
+    })
+    .catch(() => {
+      this.$message({
+        type: "info",
+        message: "已取消删除",
+      });
+    });
+};
+const changeSize = (size) => {
+  this.limit = size;
+  this.fetchData(1);
+};
+const fetchData = (page = 1) => {
+  this.page = page;
+  activitiApi
+    .getTasks(this.page, this.limit, this.searchObj)
+    .then((res) => {
+      this.list = res.data.items;
+      this.total = res.data.total;
+
+      // 数据加载并绑定成功
+      this.listLoading = false;
+    });
+};
+const resetData = () => {
+  this.searchObj = {};
+  this.fetchData();
+};
+const handleSelectionChange = (selection) => {
+  this.multipleSelection = selection;
+};
+
+onMounted(() => {
+  useApi.fetchData();
+});
+
+
+
+</script>
+
+<!--<script>
 import userApi from "@/api/acl/user";
 import activitiApi from "@/api/acl/activiti";
 import BpmnJs from "@/components/BpmnJs/index.vue";
@@ -105,11 +252,6 @@ export default {
       ifBpmnAdd: true,
       bpmnData: null,
     };
-  },
-  computed: {
-    ...mapGetters({
-      bpmnModeler: "app/bpmnModeler",
-    }),
   },
   created() {
     this.fetchData();
@@ -222,9 +364,6 @@ export default {
       this.limit = size;
       this.fetchData(1);
     },
-    addUser() {
-      this.$router.push({path: "/acl/user/add"});
-    },
     // 加载讲师列表数据
     fetchData(page = 1) {
       // 异步获取远程数据（ajax）
@@ -240,103 +379,16 @@ export default {
           this.listLoading = false;
         });
     },
-
-    // 重置查询表单
     resetData() {
       this.searchObj = {};
       this.fetchData();
     },
-
-    // 根据id删除数据
-    removeDataById(id) {
-      // debugger
-      this.$confirm("此操作将永久删除该记录, 是否继续?", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      })
-        .then(() => {
-          // promise
-          // 点击确定，远程调用ajax
-          return userApi.removeById(id);
-        })
-        .then((response) => {
-          this.fetchData(this.page);
-          if (response.success) {
-            this.$message({
-              type: "success",
-              message: "删除成功!",
-            });
-          }
-        })
-        .catch(() => {
-          this.$message({
-            type: "info",
-            message: "已取消删除",
-          });
-        });
-    },
     handleSelectionChange(selection) {
       this.multipleSelection = selection;
-    },
-    removeRows() {
-      console.log("removeRows......");
-
-      if (this.multipleSelection.length === 0) {
-        this.$message({
-          type: "warning",
-          message: "请选择要删除的记录!",
-        });
-        return;
-      }
-
-      this.$confirm("此操作将永久删除该记录, 是否继续?", "提示", {
-        confirmButtonText: "确定",
-        cancelButtonText: "取消",
-        type: "warning",
-      })
-        .then(() => {
-          // 遍历selection，将id取出放入id列表
-          var idList = [];
-          this.multipleSelection.forEach((item) => {
-            idList.push(item.id);
-            // console.log(idList)
-          });
-          // 调用api
-          return userApi.removeRows(idList);
-        })
-        .then((response) => {
-          this.fetchData(this.page);
-          if (response.success) {
-            this.$message({
-              type: "success",
-              message: "删除成功!",
-            });
-          }
-        })
-        .catch(() => {
-          this.$message({
-            type: "info",
-            message: "已取消删除",
-          });
-        });
-    },
-
-    // 执行搜索
-    // queryString：文本框中输入的值
-    // cb：一个函数
-    querySearch(queryString, cb) {
-      console.log(queryString);
-      console.log(cb);
-
-      // teacher.selectNameByKey(queryString).then(response => {
-      //   // 调用 callback 返回建议列表的数据
-      //   cb(response.data.items)
-      // })
-    },
+    }
   },
 };
-</script>
+</script>-->
 
 <style scoped>
 .dialog-layout {
