e2e343b8b46b7b6107f0c3d25415fac88ebadc25
zwxxzhl <csdhtt@foxmail.com>
2021-06-25 12:02:57 +0800

    .

--- cssysft-2146/vite.config.js
+++ cssysft-2146/vite.config.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ['bpmn-js-properties-panel']
+  },
   plugins: [vue()],
   resolve: {
     alias: {
