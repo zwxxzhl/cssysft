903671b7659e269556964880fe214cba0aaeda47
zwxxzhl <csdhtt@foxmail.com>
2021-08-19 19:43:40 +0800

    .

---
+++ iot/door-bk/src/main/java/com/zwx/doorbk/DoorBkApplication.java
@@ -0,0 +1,13 @@
+package com.zwx.doorbk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorBkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoorBkApplication.class, args);
+    }
+
+}
---
+++ iot/door-bk/src/test/java/com/zwx/doorbk/DoorBkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwx.doorbk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoorBkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
